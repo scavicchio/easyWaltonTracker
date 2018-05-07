81b98542ee229c67faf123e949e4de954632b7a9
Salvy Cavicchio <scavicchio@nyu.edu>
2018-05-06 20:41:53 -0400

    Delete waltonTracker.sql

--- waltonTracker.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE BlockChain(
-	blockNum INT,
-	miner VARCHAR,
-	extra_data VARCHAR,
-	difficulty INT,
-	timest TIMESTAMP,
-
-	PRIMARY KEY (blockNum)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
