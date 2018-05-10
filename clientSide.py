d6d038059945eada9aad4654643cb8ae44fc297d
Salvy Cavicchio <scavicchio@nyu.edu>
2018-05-10 00:57:56 -0400

    Add files via upload

--- clientSide.py
+++ clientSide.py
@@ -18,17 +18,38 @@ import pymysql.cursors
 app = Flask(__name__)
 
 #Configure MySQL
-conn = pymysql.connect(host='localhost',
-                       user='root',
+conn = pymysql.connect(host='52.14.91.37',
+                       port = 3306,
+                       user='remoteclient',
                        password='root',
                        db='waltonchain',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
+def connect():
+        conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='root',
+                       db='waltonchain',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+        return conn
+        
 #Define route for login
 @app.route('/')
 def homepage():
-	return render_template('home.html')
+        latestBlock = getLatestBlockFromDB()
+        return render_template('home.html',latestBlock=latestBlock["blockNum"])
+
+def getLatestBlockFromDB():
+    conn = connect()    
+    cursor = conn.cursor()
+    query = 'SELECT blockNum FROM BlockChain ORDER BY blockNum DESC LIMIT 1'
+    cursor.execute(query)
+    latestBlock = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return latestBlock;
 
 @app.route('/about')
 def about():
@@ -42,30 +63,35 @@ def searchMiner():
         
 @app.route('/miner/<etherbase>',methods=['GET'])
 def miner(etherbase):
+        conn = connect()
+        latestBlock = getLatestBlockFromDB()
         cursor = conn.cursor()
         query = 'SELECT * FROM BlockChain WHERE miner = %s'
         cursor.execute(query, (etherbase))
         data = cursor.fetchall()
         cursor.close()
-
+        
         cursor = conn.cursor()
         query2 = 'SELECT COUNT(*) FROM BlockChain WHERE miner = %s'
         cursor.execute(query2, (etherbase))
         data2 = cursor.fetchone()
         cursor.close()
         num = data2["COUNT(*)"]
-
+ 
         cursor = conn.cursor()
-        query3 = 'SELECT extra_data, COUNT(blockNum) AS theCount FROM blockchain WHERE miner = %s GROUP BY extra_data'
+        query3 = 'SELECT extra_data, COUNT(blockNum) AS theCount FROM BlockChain WHERE miner = %s GROUP BY extra_data'
         cursor.execute(query3,(etherbase))
         data3 = cursor.fetchall()
         cursor.close()
-        print(data3)
 
-        return render_template('miner.html',data=data,etherbase=etherbase,blockCount=num,data3=data3)
+        conn.close()
+
+        return render_template('miner.html',latestBlock=latestBlock["blockNum"],data=data,etherbase=etherbase,blockCount=num,data3=data3)
 
 @app.route('/miner/<etherbase>/<key>',methods=['GET'])
 def minerKey(etherbase,key):
+        conn = connect()
+        latestBlock = getLatestBlockFromDB()
         print("will grab data for below etherbase:")
         print(etherbase)
         cursor = conn.cursor()
@@ -73,9 +99,9 @@ def minerKey(etherbase,key):
         cursor.execute(query, (etherbase))
         data = cursor.fetchall()
         cursor.close()
-
-        print(data)
-        return render_template('miner.html',data=data,etherbase=etherbase)
+        conn.close()
+        
+        return render_template('miner.html',latestBlock=latestBlock["blockNum"],data=data,etherbase=etherbase)
 
 if __name__ == "__main__":
 	app.run('127.0.0.1', 5000, debug = True)
